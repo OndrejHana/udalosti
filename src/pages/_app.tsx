c2bec164a8d9a42f78b1180a3250f572cf5e74a9
OndrejHana <ondrahana001@gmail.com>
2023-03-24 11:43:13 +0100

    ganalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(<>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-S6LXJBNV29"/>
+      <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-S6LXJBNV29');
+        `
+      }}
+      />
+
+   <Component {...pageProps} />
+   </>);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div>
+
       <h1>Dobřichovická alotria</h1>
       <p>Přiďte se zůčastnit jubilejního 20. ročníku jednoho z největších historických a šermířských festivalů v Čechách</p>
     </div>
