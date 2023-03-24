6383e39f2ec20bee5c942864aa0fc8b7803a252a
OndrejHana <ondrahana001@gmail.com>
2023-03-24 11:03:30 +0100

    Initial commit from Create Next App

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import banner from "../../public/baner_web_Alotria_2018.jpg";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Dobřichovická alotria</h1>
+      <p>Přiďte se zůčastnit jubilejního 20. ročníku jednoho z největších historických a šermířských festivalů v Čechách</p>
+    </div>
+  );
+}
