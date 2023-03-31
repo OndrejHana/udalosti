5c24943fed5b0ba51d818893a6d05b66ffb1c8f2
Ondřej Hána <ondrahana001@gmail.com>
2023-03-31 08:25:49 +0000

    added contact information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import banner from 'public/banner.jpg';
 
 export default function Home() {
   return (
-    <div>
-      <Image src={banner} alt="banner" className='banner'/>
-      <h1>Dobřichovická alotria</h1>
-      <p>Přiďte se zůčastnit jubilejního 20. ročníku jednoho z největších historických a šermířských festivalů v Čechách</p>
-    </div>
-  );
+    <>
+      <div>
+        <Image src={banner} alt="banner" className='banner'/>
+        <h1>Dobřichovická alotria</h1>
+        <p>Přiďte se zůčastnit jubilejního 20. ročníku jednoho z největších historických a šermířských festivalů v Čechách</p>
+      </div>
+      <div className="contact">
+        <p>Kontakt: hana@spsejecna.cz</p>
+      </div>    
+    </>
+    );
 }
