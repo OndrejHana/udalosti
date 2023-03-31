292f3f86eb27819699c40ac12aa2401205cb1b71
Ondřej Hána <ondrahana001@gmail.com>
2023-03-31 08:06:05 +0000

    updated style and banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+import banner from 'public/banner.jpg';
+
 export default function Home() {
   return (
     <div>
-
+      <Image src={banner} alt="banner" className='banner'/>
       <h1>Dobřichovická alotria</h1>
       <p>Přiďte se zůčastnit jubilejního 20. ročníku jednoho z největších historických a šermířských festivalů v Čechách</p>
     </div>
